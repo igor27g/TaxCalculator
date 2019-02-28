7e9a011f5c7b1a8f9def4882801387fd09847b99
igor27w <igor.piotr.grabowski@gmail.com>
2019-02-28 19:59:20 +0100

    First commit

---
+++ PropertyTaxCalc1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PropertyTaxCalc1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PropertyTaxCalc1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18870e8c-36ac-4140-8cae-0f4a1ae66fe5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PropertyTaxCalc1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PropertyTaxCalc1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void Test_Calculate_Taxable_Value()
+		{
+			PropertyTaxCalc home1 = new PropertyTaxCalc("123 New Street", 110000, 115000);
+			Assert.AreEqual(90000, home1.CalculateTaxableValue());
+		}
+
+	
+
+		[TestMethod]
+		public void Test_Class_Output()
+		{
+			PropertyTaxCalc home1 = new PropertyTaxCalc("123 New Street", 110000, 115000);
+			Console.WriteLine(home1);
+
+
+			PropertyTaxCalc home2 = new PropertyTaxCalc("456 Long Street", 150000, 160000);
+			Console.WriteLine(home2);
+
+
+		}
+
+
+
+	}
+}
